f134b3ec1132460b252ea2322d50768460333d71
이희수 <leehsdd33@gmail.com>
2025-01-27 17:14:04 +0900

    feat: 패키지 생성

--- src/main/java/com/example/scheduler/controller/EventController.java
+++ src/main/java/com/example/scheduler/controller/EventController.java
@@ -1,7 +1,16 @@
 package com.example.scheduler.controller;
 
+import com.example.scheduler.service.EventService;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController   // JSON 형태로 데이터 통신
+@RestController   // JSON 형태로 데이터 통신  // @Controller + @ResponseBody
+@RequestMapping("/events") // prefix
 public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
 }
---
+++ src/main/java/com/example/scheduler/repository/EventRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.example.scheduler.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EventRepositoryImpl implements EventRepository {
+}
